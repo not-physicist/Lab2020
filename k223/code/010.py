53a74372322c083cbb7d7d2c344e6a977ea38d31
Chenhuan Wang <chenhuan@fastmail.com>
2020-08-05 17:37:49 +0200

    update k223 folder structure, template, some theory part

---
+++ k223/code/010.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def W(theta):
+    return 0.949 + 0.153*np.cos(theta)**2
+
+thetaArray = np.linspace(0,np.pi, 100)
+WArray = W(thetaArray)
+
+k=0
+plt.figure(num=k)
+plt.plot(thetaArray, WArray)
+plt.ylabel(r"$W(\theta)$")
+plt.xlabel(r"$\theta$")
+
+plt.savefig("../report/figs/010.pdf", bbox_inches="tight")
+plt.close(k)
+k += 1
